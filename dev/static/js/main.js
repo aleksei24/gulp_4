afa7db2727ea467a2453036be6d0fa3533c0e154
aleksei24 <lexa24032@gmail.com>
2020-03-04 21:03:40 +0300

    engine for mobile menu has been launched

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -47,3 +47,13 @@ if (!Array.from) {
         return [].slice.call(object);
     };
 }
+
+/* Open when someone clicks on the span element */
+function openMenu() {
+    document.getElementById("mobile").style.width = "100%";
+}
+
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+function closeMenu() {
+    document.getElementById("mobile").style.width = "0%";
+}
