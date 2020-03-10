09ee61923124c2a7a4a7e8b6221630afa7b3df98
aleksei24 <lexa24032@gmail.com>
2020-03-10 16:28:32 +0300

    task for html and code is being assembled in /out

--- gulpfile.js
+++ gulpfile.js
@@ -10,16 +10,23 @@ gulp.task('scss', function(){
         .pipe(browserSync.reload({stream: true}))
 });
 
+gulp.task('html', function(){
+    return gulp.src('app/**/*.html')
+        .pipe(gulp.dest('out'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: "app/"
+            baseDir: "out/"
         }
     });
 });
 
 gulp.task('watch', function(){
-    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
+    gulp.watch('app/**/*.html', gulp.parallel('html'))
 });
 
 gulp.task('default', gulp.parallel('browser-sync', 'watch'));
