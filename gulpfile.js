9bd2b8f158bad4958cd64a149e3b2f4712cabd9b
aleksei24 <lexa24032@gmail.com>
2020-03-10 12:57:11 +0300

    task for scss has been set

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
-const gulp = require('gulp');
+const gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+
+gulp.task('scss', function(){
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('out/css'))
+});
+
