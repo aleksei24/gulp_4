9134b318bb54577cf9867c1a2c3322fbe4de8799
aleksei24 <lexa24032@gmail.com>
2020-03-10 18:07:07 +0300

    new plugins have been set

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 const gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create(),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename');
 
 
 gulp.task('scss', function(){
     return gulp.src('app/scss/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('out/css'))
         .pipe(browserSync.reload({stream: true}))
 });
@@ -13,6 +17,23 @@ gulp.task('scss', function(){
 gulp.task('html', function(){
     return gulp.src('app/**/*.html')
         .pipe(gulp.dest('out'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('script', function(){
+    return gulp.src('app/js/**/*.js')
+        .pipe(gulp.dest('out/js'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('js', function(){
+    return gulp.src([
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+    ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('out/js'))
         .pipe(browserSync.reload({stream: true}))
 });
 
@@ -26,7 +47,8 @@ gulp.task('browser-sync', function() {
 
 gulp.task('watch', function(){
     gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
-    gulp.watch('app/**/*.html', gulp.parallel('html'))
+    gulp.watch('app/**/*.html', gulp.parallel('html'));
+    gulp.watch('app/js/**/*.js', gulp.parallel('script'));
 });
 
-gulp.task('default', gulp.parallel('browser-sync', 'watch'));
+gulp.task('default', gulp.parallel('scss', 'js', 'browser-sync', 'watch'));
