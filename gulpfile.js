cef9d0024b2439615fcc65071f982b199b765554
aleksei24 <lexa24032@gmail.com>
2020-05-08 13:20:16 +0300

    plugin gulp-sass has been pulled and put (OK)

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,8 @@ let { src, dest } = require('gulp'),
     gulp = require('gulp'),
     browser = require('browser-sync').create(),
     fileinclude = require('gulp-file-include'),
-    del = require('del');
+    del = require('del'),
+    scss = require('gulp-sass');
 
 function browserSync(par) {
     browser.init({
@@ -49,20 +50,29 @@ function html() {
 }
 
 function css(params) {
-    return src(path.src.css).pipe(dest(path.build.css)).pipe(browser.stream());
+    return src(path.src.css)
+        .pipe(
+            scss({
+                outputStyle: 'expanded',
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(browser.stream());
 }
 
 function watchFiles(params) {
     gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], css);
 }
 
 function clean(params) {
     return del(path.clean);
 }
 
-let build = gulp.series(clean, html);
+let build = gulp.series(clean, gulp.parallel(css, html));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.css = css;
 exports.html = html;
 exports.build = build;
 exports.watch = watch;
