b3e4fd2a13f076f5ca601a47a88c38afabdce50c
aleksei24 <lexa24032@gmail.com>
2020-05-08 11:58:50 +0300

    ways to necessary folders have been added

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,26 @@
 const projectFolder = 'dist',
     sourceFolder = 'app';
+
+let path = {
+    build: {
+        html: projectFolder + '/',
+        css: projectFolder + '/css/',
+        js: projectFolder + '/js/',
+        img: projectFolder + '/img/',
+        fonts: projectFolder + '/fonts/',
+    },
+    src: {
+        html: sourceFolder + '/',
+        css: sourceFolder + '/scss/style.scss',
+        js: sourceFolder + '/js/script.js',
+        img: sourceFolder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
+        fonts: sourceFolder + '/fonts/*.{woff,woff2}',
+    },
+    watch: {
+        html: sourceFolder + '/**/*.html',
+        css: sourceFolder + '/scss/**/*.scss',
+        js: sourceFolder + '/js/**/*.js',
+        img: sourceFolder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
+    },
+    clean: './' + projectFolder + '/',
+};
