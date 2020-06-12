03266de0d8e7becb85adbc0746dba1bdd2797c09
aleksei24 <lexa24032@gmail.com>
2020-06-12 17:23:41 +0300

    engine for burger menu has been fired up

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,7 @@
-// put @@ before include (without space)
-// include('alert.js'); // don`t worry, it`s not an error
+// burger menu
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('locked');
+    });
+});
