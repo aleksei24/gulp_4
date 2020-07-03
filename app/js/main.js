495993cc11a9b228fbd20ab16d209ca8cf72294a
aleksei24 <lexa24032@gmail.com>
2020-07-03 11:34:59 +0300

    slider inside slider has been made

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,32 @@ $(document).ready(function () {
         $('body').toggleClass('locked');
     });
 });
+// slider
+$('.slider__img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    infinite: true,
+    fade: true,
+    asNavFor: '.slider__row',
+    responsive: [
+        {
+            breakpoint: 376,
+            settings: {
+                autoplay: true,
+                autoplaySpeed: 2500,
+                arrows: false,
+            },
+        },
+    ],
+});
+$('.slider__row').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    asNavFor: '.slider__img',
+    dots: false,
+    infinite: true,
+    arrows: false,
+    centerMode: true,
+    focusOnSelect: true,
+});
