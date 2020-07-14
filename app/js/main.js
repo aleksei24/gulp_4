54a6de060550224f8666eaac87baad609edf49e1
aleksei24 <lexa24032@gmail.com>
2020-07-14 16:27:00 +0300

    an engine for custom select has been fired up

--- app/js/main.js
+++ app/js/main.js
@@ -71,3 +71,35 @@ $('.column__pics').magnificPopup({
         preload: [1, 1], // Will preload 0 - before current, and 1 after the current image
     },
 });
+
+// custom select
+$('.drop .option').click(function () {
+    let val = $(this).attr('data-value'),
+        $drop = $('.drop'),
+        prevActive = $('.drop .option.active').attr('data-value'),
+        options = $('.drop .option').length;
+    $drop.find('.option.active').addClass('mini-hack');
+    $drop.toggleClass('visible');
+    $drop.removeClass('withBG');
+    $(this).css('top');
+    $drop.toggleClass('opacity');
+    $('.mini-hack').removeClass('mini-hack');
+    if ($drop.hasClass('visible')) {
+        setTimeout(function () {
+            $drop.addClass('withBG');
+        }, 400 + options * 100);
+    }
+    triggerAnimation();
+    if (val !== 'placeholder' || prevActive === 'placeholder') {
+        $('.drop .option').removeClass('active');
+        $(this).addClass('active');
+    }
+});
+
+function triggerAnimation() {
+    let finalWidth = $('.drop').hasClass('visible') ? 22 : 20;
+    $('.drop').css('width', '24em');
+    setTimeout(function () {
+        $('.drop').css('width', finalWidth + 'em');
+    }, 400);
+}
