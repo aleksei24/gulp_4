f1baeb5e9fde1f9f18a4e18026c2dc2710ece4bd
aleksei24 <lexa24032@gmail.com>
2020-09-14 15:39:19 +0300

    still trying

--- app/js/main.js
+++ app/js/main.js
@@ -53,7 +53,16 @@ if (mobile.any()) {
 //=================================================================================================================
 // slider
 window.addEventListener('load', function () {
-    new Glider(document.querySelector('.slider__img', {}));
+    new Glider(
+        document.querySelector('.slider__img', {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: {
+                prev: '.glider-prev',
+                next: '.glider-next',
+            },
+        })
+    );
 });
 // =================================================================================================================
 // main-popup
@@ -64,12 +73,6 @@ window.addEventListener('load', function () {
 //     $('.order').removeClass('active');
 // });
 
-$('#mobId').click(function (event) {
-    $('.order').addClass('mob');
-    $('.order__close').click(function () {
-        $('.order').removeClass('mob');
-    });
-});
 // ====================================================================================================================
 // fixed menu
 // $(window).scroll(function () {
@@ -78,139 +81,4 @@ $('#mobId').click(function (event) {
 //     } else {
 //         $('.header').removeClass('header_fixed');
 //     }
-// });
-// ================================================================================================================
-// magnific
-// $('.column__pics').magnificPopup({
-//     delegate: 'a',
-//     type: 'image',
-//     tLoading: 'Loading image #%curr%...',
-//     mainClass: 'mfp-img-mobile',
-//     gallery: {
-//         enabled: true,
-//         navigateByImgClick: true,
-//         preload: [1, 1], // Will preload 0 - before current, and 1 after the current image
-//     },
-// });
-
-// ==================================================================================================================
-// select calculation
-// $('.calc__select').each(function () {
-//     // Variables
-//     let $this = $(this),
-//         selectOption = $this.find('option'),
-//         selectOptionLength = selectOption.length,
-//         selectedOption = selectOption.filter(':selected'),
-//         dur = 200;
-
-//     $this.hide();
-//     // Wrap all in select box
-//     $this.wrap('<div class="calc__select"></div>');
-//     // Style box
-//     $('<div>', {
-//         class: 'select__gap',
-//         text: 'Please select', //Placeholder
-//     }).insertAfter($this);
-
-//     let selectGap = $this.next('.select__gap'),
-//         caret = selectGap.find('.caret');
-//     // Add ul list
-//     $('<ul>', {
-//         class: 'select__list',
-//     }).insertAfter(selectGap);
-
-//     let selectList = selectGap.next('.select__list');
-//     // Add li - option items
-//     for (let i = 0; i < selectOptionLength; i++) {
-//         $('<li>', {
-//             class: 'select__item',
-//             html: $('<span>', {
-//                 text: selectOption.eq(i).text(),
-//             }),
-//         })
-//             .attr('data-value', selectOption.eq(i).val())
-//             .appendTo(selectList);
-//     }
-//     // Find all items
-//     let selectItem = selectList.find('li');
-
-//     selectList.slideUp(0);
-//     selectGap.on('click', function () {
-//         if (!$(this).hasClass('on')) {
-//             $(this).addClass('on');
-//             selectList.slideDown(dur);
-//             selectItem.on('click', function () {
-//                 let chooseItem = $(this).data('value');
-//                 $('select').val(chooseItem).attr('selected', 'selected');
-//                 selectGap.text($(this).find('span').text());
-//                 selectList.slideUp(dur);
-//                 selectGap.removeClass('on');
-//             });
-//         } else {
-//             $(this).removeClass('on');
-//             selectList.slideUp(dur);
-//         }
-//     });
-// });
-// ==================================================================================================================
-// calculation itself
-// $('.calc__form select').change(function () {
-//     $edition = $('#edition').val();
-//     $paper = $('#paper').val();
-//     $paperRatio = $('#paper option:selected').attr('data-paper');
-//     $colour = $('#colour').val();
-//     $print = $('#colour option:selected').attr('data-print');
-//     $kind = $('#colour option:selected').attr('data-kind');
-//     $card = $('#colour option:selected').attr('data-colour');
-//     if ($card == 01) {
-//         $('.calc__img img').hide();
-//         $('#card-1').show(600);
-//     }
-//     if ($card == 02) {
-//         $('.calc__img img').hide();
-//         $('#card-2').show(600);
-//     }
-//     if ($card == 03) {
-//         $('.calc__img img').hide();
-//         $('#card-3').show(600);
-//     }
-//     if ($card == 04) {
-//         $('.calc__img img').hide();
-//         $('#card-4').show(600);
-//     }
-//     if ($card == 05) {
-//         $('.calc__img img').hide();
-//         $('#card-5').show(600);
-//     }
-//     $division = 30;
-//     $final = $edition / $division;
-//     $price = $edition * $paperRatio + parseInt($print) + $final * $print;
-//     $finalPrice = $('span#final-price');
-//     $finalPrice.text($price.toFixed(0));
-//     console.log($price);
-// });
-// // ====================================================================================================================
-// // form counter
-// DecreaseCount = function () {
-//     let count = $('.counter__display').val();
-//     if (count > 1) count--;
-//     if (count == 1) count = 1;
-//     $('.counter__display').val(count);
-//     console.log(count);
-// };
-
-// IncreaseCount = function () {
-//     let count = $('.counter__display').val();
-//     if (count < 10) count++;
-//     if (count == 10) count = 10;
-//     $('.counter__display').val(count);
-//     console.log(count);
-// };
-
-// $('.minus').click(function () {
-//     DecreaseCount();
-// });
-
-// $('.plus').click(function () {
-//     IncreaseCount();
 // });
