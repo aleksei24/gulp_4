f92588d4cf7a5c19c9643ec25c12a74d63c15b5b
aleksei24 <lexa24032@gmail.com>
2020-10-12 16:20:16 +0300

    magnific engine has been launched

--- app/js/main.js
+++ app/js/main.js
@@ -61,17 +61,17 @@ $(window).scroll(function () {
 });
 // ================================================================================================================
 // magnific
-// $('.column__pics').magnificPopup({
-//     delegate: 'a',
-//     type: 'image',
-//     tLoading: 'Loading image #%curr%...',
-//     mainClass: 'mfp-img-mobile',
-//     gallery: {
-//         enabled: true,
-//         navigateByImgClick: true,
-//         preload: [1, 1], // Will preload 0 - before current, and 1 after the current image
-//     },
-// });
+$('.column__pics').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [1, 1], // Will preload 0 - before current, and 1 after the current image
+    },
+});
 // ========================================================================================================================
 // custom select
 $('.drop .option').click(function () {
