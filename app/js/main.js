b087abd5838c3078addaee38bd6bc001ae201f5a
aleksei24 <lexa24032@gmail.com>
2021-01-14 17:46:37 +0300

    a single select has been made

--- app/js/main.js
+++ app/js/main.js
@@ -470,14 +470,33 @@ customSelectBlock.forEach(function (everyCustomSelect) {
 // ====================================================================================================
 // single custom select
 if (singleCustomSelect) {
-    let singleSelect = function () {
-        let selectTop = document.querySelectorAll('.single-select__top');
-        selectTop.forEach((el) => {
-            el.addEventListener('click', function () {
-                this.parentElement.classList.toggle('active');
-            });
+    const customSelectButton = document.querySelector('.single-select__top');
+    const customSelectList = document.querySelector('.single-select__list');
+    const customSelectItem = customSelectList.querySelectorAll(
+        '.single-select__item'
+    );
+    const customSelectInput = document.querySelector(
+        '.single-select__input_hidden'
+    );
+
+    customSelectButton.addEventListener('click', function () {
+        customSelectList.classList.toggle('dropdown__list_visible');
+        this.classList.add('active');
+    });
+    customSelectItem.forEach(function (item) {
+        item.addEventListener('click', function (e) {
+            e.stopPropagation();
+            customSelectButton.innerText = this.innerText;
+            customSelectInput.value = this.dataset.value;
+            customSelectList.classList.remove('dropdown__list_visible');
         });
-    };
+    });
+    document.addEventListener('click', function (e) {
+        if (e.target !== customSelectButton) {
+            customSelectButton.classList.remove('active');
+            customSelectList.classList.remove('dropdown__list_visible');
+        }
+    });
 }
 
 // =================================================================================================
