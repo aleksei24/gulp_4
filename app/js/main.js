af72cc69aae1e9b1918664ca5564cd9d81de391b
aleksei24 <lexa24032@gmail.com>
2021-01-27 10:48:14 +0300

    new tabs have been set

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,8 @@
 // default variables
 const header = document.querySelector('.header');
 const popup = document.querySelector('.popup');
-const tabsContainer = document.querySelector('.tabs');
+const tabsContainer = document.querySelector('.tabs-page');
+const pureTabs = document.querySelector('.pure-tabs');
 const catalogueFilter = document.querySelectorAll('.catalogue-filter');
 const foldAside = document.querySelector('.fold-aside');
 const catCols = document.querySelector('.catalogue-cols__list');
@@ -312,7 +313,7 @@ function fixedHeader() {
 }
 
 // ================================================================================================
-// tabs JS
+// tabs page
 const tabsControls = [].slice.call(document.querySelectorAll('.control'));
 
 if (tabsContainer) {
@@ -684,3 +685,51 @@ document.addEventListener('DOMContentLoaded', function () {
         showImage.readAsDataURL(file);
     }
 });
+// ========================================================================================================
+// tabs
+if (pureTabs) {
+    tabs();
+}
+function tabs() {
+    const top = document.querySelector('.pure-tabs__top');
+    const body = document.querySelector('.pure-tabs__body');
+
+    const getActiveTabName = () => {
+        return top.querySelector('.pure-tabs__caption_active').dataset.tab;
+    };
+
+    const setActiveContent = () => {
+        if (body.querySelector('.pure-tabs__content_active')) {
+            body.querySelector('.pure-tabs__content_active').classList.remove(
+                'pure-tabs__content_active'
+            );
+        }
+        body.querySelector(`[data-tab=${getActiveTabName()}]`).classList.add(
+            'pure-tabs__content_active'
+        );
+    };
+
+    if (!top.querySelector('.pure-tabs__caption_active')) {
+        top.querySelector('.pure-tabs__caption').classList.add(
+            'pure-tabs__caption_active'
+        );
+    }
+
+    setActiveContent(getActiveTabName());
+
+    top.addEventListener('click', (e) => {
+        const caption = e.target.closest('.pure-tabs__caption');
+        if (!caption) return;
+        if (caption.classList.contains('pure-tabs__caption_active')) return;
+
+        if (top.querySelector('.pure-tabs__caption_active')) {
+            top.querySelector('.pure-tabs__caption_active').classList.remove(
+                'pure-tabs__caption_active'
+            );
+        }
+
+        caption.classList.add('pure-tabs__caption_active');
+
+        setActiveContent(getActiveTabName());
+    });
+}
