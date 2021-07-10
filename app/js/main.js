dc4df52e004b880d159612e70b2959ad88954144
aleksei24 <lexa24032@gmail.com>
2021-07-10 22:53:15 +0300

    chunks are linked

--- app/js/main.js
+++ app/js/main.js
@@ -1,77 +1,10 @@
 // default variables
-const header = document.querySelector('.header');
-const tabsContainer = document.querySelector('.tabs-page');
 const catalogueFilter = document.querySelectorAll('.catalogue-filter');
 const foldAside = document.querySelector('.fold-aside');
 const catalogGoodsGrid = document.querySelector('.catalogue-goods__grid');
-const customSelect = document.querySelectorAll('.cust-sel');
-const catalogueTopCloseBtn = document.querySelector('.top__close');
 const headerTop = document.querySelector('.header_top');
-const goodsGrid = document.querySelector('.tab');
-const loadMore = document.querySelector('.tab__show-more');
-const toTop = document.querySelector('.toTop');
 
-// =================================================================================================
-// custom-select 2
-if (customSelect) {
-    customSelect.forEach((elem) => {
-        elem.addEventListener('click', (e) => {
-            e.currentTarget.classList.toggle('cust-sel_active');
-            if (e.target.classList.contains('cust-sel__item')) {
-                let text = e.target.textContent;
-                e.currentTarget.querySelector('.cust-sel__top').textContent = text;
-            }
-        });
-    });
-}
-// =================================================================================================
-// catalogue top
-if (catalogueTopCloseBtn) {
-    catalogueTopCloseBtn.addEventListener('click', (el) => {
-        el.currentTarget.closest('.top').style.display = 'none';
-    });
-}
-// =================================================================================================
-// fixed menu
-if (header) {
-    window.onscroll = function () {
-        fixedHeader();
-    };
-}
 
-if (header.classList.contains('header_top')) {
-    header.style.top = -40;
-} else {
-    header.style.top = 0;
-}
-
-function fixedHeader() {
-    if (window.pageYOffset >= 50) {
-        header.classList.add('header_fixed');
-    } else {
-        header.classList.remove('header_fixed');
-    }
-}
-
-// ================================================================================================
-// tabs page
-const tabsControls = [].slice.call(document.querySelectorAll('.control'));
-
-if (tabsContainer) {
-    let setTab = function () {
-        let num = tabsControls.indexOf(this);
-        tabsContainer.style.left = `${-num * 100}%`;
-
-        for (let control of tabsControls) {
-            control.classList.remove('active');
-        }
-        this.classList.add('active');
-    };
-
-    for (let control of tabsControls) {
-        control.addEventListener('click', setTab);
-    }
-}
 // =================================================================================================
 // aside
 catalogueFilter.forEach((elem) => {
@@ -88,122 +21,6 @@ if (foldAside) {
     });
 }
 
-// =================================================================================================
-// upTo
-if (toTop) {
-    window.onscroll = function () {
-        scrollFunction();
-        fixedHeader();
-    };
-
-    toTop.addEventListener('click', onTheTop);
-}
-
-function scrollFunction() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        toTop.classList.add('show');
-    } else {
-        toTop.classList.remove('show');
-    }
-}
-
-function onTheTop() {
-    let position = document.body.scrollTop !== 0 || document.documentElement.scrollTop !== 0;
-    if (position) {
-        window.scrollBy(0, -40);
-        requestAnimationFrame(onTheTop);
-    }
-}
-
-// in window.scrollBy(0, -30), changing a second parameter(-30) affects the speed of scrolling
-// less than -30 means faster speed
-// greater than -30 means slower speed
-// a second parameter must be negative, otherwise it won`t work
-
-// =================================================================================================
-// custom-select
-// for only one custom-select element unwrap the code out of block.forEach
-const customSelectBlock = document.querySelectorAll('.custom-select');
-customSelectBlock.forEach(function (everyCustomSelect) {
-    const customSelectButton = everyCustomSelect.querySelector('.custom-select__btn');
-    const customSelectButtonSpan = everyCustomSelect.querySelector('.custom-select__btn span');
-    const customSelectList = everyCustomSelect.querySelector('.custom-select__list');
-    const customSelectItem = customSelectList.querySelectorAll('.custom-select__item');
-    const customSelectInput = everyCustomSelect.querySelector('.custom-select__input_hidden');
-
-    customSelectButton.addEventListener('click', function () {
-        customSelectList.classList.toggle('dropdown__list_visible');
-        this.classList.add('custom-select-active');
-    });
-    customSelectItem.forEach(function (item) {
-        item.addEventListener('click', function (e) {
-            e.stopPropagation();
-            customSelectButton.innerText = this.innerText;
-            customSelectInput.value = this.dataset.value;
-            customSelectList.classList.remove('dropdown__list_visible');
-        });
-    });
-    document.addEventListener('click', function (e) {
-        if (e.target !== customSelectButton) {
-            customSelectButton.classList.remove('custom-select-active');
-            customSelectList.classList.remove('dropdown__list_visible');
-        }
-    });
-    /*document.addEventListener('keydown', function (e) {
-        if (e.key === 'Tab' || e.key === 'Escape') {
-            customSelectButton.classList.remove('custom-select-active');
-            customSelectList.classList.remove('dropdown__list_visible');
-        }
-    });*/
-});
-// if the code is going to be used in old browsers
-// you should add a polyfill below
-// if (window.NodeList && !NodeList.prototype.forEach) {
-//     NodeList.prototype.forEach = function (callback, thisArg) {
-//         thisArg = thisArg || window;
-//         for (var i = 0; i < this.length; i++) {
-//             callback.call(thisArg, this[i], i, this);
-//         }
-//     };
-// }
-
-// ====================================================================================================
-// show more
-if (goodsGrid && loadMore) {
-    const fetchGoods = (line = 3) => {
-        fetch('js/goods.json')
-            .then((resp) => {
-                return resp.json();
-            })
-            .then((data) => {
-                for (let i = 0; i < data.length; i++) {
-                    if (i < line) {
-                        goodsGrid.innerHTML += `
-                            <div class="trainers__card">
-                                <a href="#!" class="trainers__img">
-                                    <img src="${data[i].source}" alt="${data[i].title}" />
-                                </a>
-                                <div class="trainers__descript">
-                                    <h3 class="trainers__headline">${data[i].title}</h3>
-                                    <p class="trainers__text">
-                                        Our staff includes amazing professionals that have
-                                        an eye for crossfit.
-                                    </p>
-                                    <span>${data[i].price}</span>
-                                </div>
-                            </div>
-                        `;
-                    }
-                }
-            });
-    };
-    let amountOfGoods = 3;
-    // fetchGoods(amountOfGoods);
-    loadMore.addEventListener('click', (e) => {
-        amountOfGoods += 3;
-        fetchGoods(amountOfGoods);
-    });
-}
 // ====================================================================================
 
 
@@ -223,3 +40,9 @@ if (goodsGrid && loadMore) {
 @@include('./chunks/_swiper_slider.js')
 @@include('./chunks/_colour_select.js')
 @@include('./chunks/_size_select.js')
+@@include('./chunks/_custom_select.js')
+@@include('./chunks/_tabs_page.js')
+@@include('./chunks/_advert_top.js')
+@@include('./chunks/_fixed_header.js')
+@@include('./chunks/_up_to.js')
+@@include('./chunks/_show_more.js')
