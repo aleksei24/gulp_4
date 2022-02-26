3156e0d736504fd3803b742be515f0904e4a14c6
aleksei24 <lexa24032@gmail.com>
2022-02-26 14:06:57 +0300

    rewriting to relative units

--- app/js/chunks/_side_popup.js
+++ app/js/chunks/_side_popup.js
@@ -5,42 +5,42 @@ const closePopup = document.querySelector('.side-popup__close');
 const popupTitle = document.querySelector('.side-popup__title');
 const popupWhen = document.querySelector('.side-popup__from');
 const popupData = [
-    {
-        title: 'A coat',
-        when: 'A minute ago',
-    },
-    {
-        title: 'A dress',
-        when: '30 seconds ago',
-    },
-    {
-        title: 'A suit',
-        when: 'Now',
-    },
+  {
+    title: 'A coat',
+    when: 'A minute ago',
+  },
+  {
+    title: 'A dress',
+    when: '30 seconds ago',
+  },
+  {
+    title: 'A suit',
+    when: 'Now',
+  },
 ];
 let popupCount = 0;
 let popupDelay = 90000;
 let exposeFor = 4000;
 if (popup) {
-    closePopup.addEventListener('click', function () {
-        popup.classList.add('remove');
-    });
+  closePopup.addEventListener('click', function () {
+    popup.classList.add('remove');
+  });
 
-    setInterval(changPopupData, popupDelay);
+  setInterval(changPopupData, popupDelay);
 }
 
 function changPopupData() {
-    popup.classList.remove('active');
-    popup.classList.remove('remove');
-    setTimeout(() => {
-        popup.classList.add('active');
-    }, popupDelay - exposeFor);
-    const titlePopup = `${popupData[popupCount].title}`;
-    const timePopup = `${popupData[popupCount].when}`;
-    popupTitle.textContent = titlePopup;
-    popupWhen.textContent = timePopup;
-    popupCount++;
-    if (popupCount === popupData.length) {
-        popupCount = 0;
-    }
+  popup.classList.remove('active');
+  popup.classList.remove('remove');
+  setTimeout(() => {
+    popup.classList.add('active');
+  }, popupDelay - exposeFor);
+  const titlePopup = `${popupData[popupCount].title}`;
+  const timePopup = `${popupData[popupCount].when}`;
+  popupTitle.textContent = titlePopup;
+  popupWhen.textContent = timePopup;
+  popupCount++;
+  if (popupCount === popupData.length) {
+    popupCount = 0;
+  }
 }
