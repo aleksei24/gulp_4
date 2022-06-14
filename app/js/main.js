29ce2e9865d8de40ff08fa755605e25d9993d68c
Aleksei <lexa24032@gmail.com>
2022-06-14 16:51:51 +0300

    main.js has been rewritten due to rules

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,23 @@
-
-// ===============================================================
-
-// the code below works well, though errors are underlined
-@@include('./chunks/_hi.js')
-@@include('./chunks/_card_slider.js')
-@@include('./chunks/_filter.js')
-@@include('./chunks/_accordion.js')
-@@include('./chunks/_tabs.js')
-@@include('./chunks/_form.js')
-@@include('./chunks/_counter.js')
-@@include('./chunks/_burger.js')
-@@include('./chunks/_side_popup.js')
-@@include('./chunks/_main_popup.js')
-@@include('./chunks/_columns_number.js')
-@@include('./chunks/_single_custom_select.js')
-@@include('./chunks/_swiper_slider.js')
-@@include('./chunks/_colour_select.js')
-@@include('./chunks/_size_select.js')
-@@include('./chunks/_spoilers.js')
-@@include('./chunks/_custom_select.js')
-@@include('./chunks/_tabs_page.js')
-@@include('./chunks/_dynamic_adapt.js')
-@@include('./chunks/_advert_top.js')
-@@include('./chunks/_fixed_header.js')
-@@include('./chunks/_up_to.js')
-@@include('./chunks/_show_more.js')
-@@include('./chunks/_aside.js')
+import './chunks/_hi';
+import './chunks/_fixed_header';
+import './chunks/_card_slider';
+import './chunks/_filter';
+import './chunks/_accordion';
+import './chunks/_tabs';
+import './chunks/_form';
+import './chunks/_counter';
+import './chunks/_burger';
+import './chunks/_side_popup';
+import './chunks/_main_popup';
+import './chunks/_columns_number';
+import './chunks/_single_custom_select';
+import './chunks/_swiper_slider';
+import './chunks/_colour_select';
+import './chunks/_size_select';
+import './chunks/_custom_select';
+import './chunks/_tabs_page';
+import './chunks/_dynamic_adapt';
+import './chunks/_advert_top';
+import './chunks/_up_to';
+import './chunks/_show_more';
+import './chunks/_aside';
